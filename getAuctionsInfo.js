db2f60b2ec000f09fb10c976a4f437286c7772ea
Chirag Mahaveer Parmar <chiragparmar12209@gmail.com>
2018-08-04 00:33:09 +0530

    Storing data as jsin objects

--- getAuctionsInfo.js
+++ getAuctionsInfo.js
@@ -17,6 +17,53 @@ registrar = "0x6090A6e47849629b7245Dfa1Ca21D94cd15878Ef"
 const registrarABI = [{"constant":false,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"releaseDeed","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"getAllowedTime","outputs":[{"name":"timestamp","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"unhashedName","type":"string"}],"name":"invalidateName","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"hash","type":"bytes32"},{"name":"owner","type":"address"},{"name":"value","type":"uint256"},{"name":"salt","type":"bytes32"}],"name":"shaBid","outputs":[{"name":"sealedBid","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"bidder","type":"address"},{"name":"seal","type":"bytes32"}],"name":"cancelBid","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"entries","outputs":[{"name":"","type":"uint8"},{"name":"","type":"address"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"ens","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_hash","type":"bytes32"},{"name":"_value","type":"uint256"},{"name":"_salt","type":"bytes32"}],"name":"unsealBid","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"transferRegistrars","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"bytes32"}],"name":"sealedBids","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"state","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_hash","type":"bytes32"},{"name":"newOwner","type":"address"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_hash","type":"bytes32"},{"name":"_timestamp","type":"uint256"}],"name":"isAllowed","outputs":[{"name":"allowed","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"finalizeAuction","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"registryStarted","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"launchLength","outputs":[{"name":"","type":"uint32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"sealedBid","type":"bytes32"}],"name":"newBid","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"labels","type":"bytes32[]"}],"name":"eraseNode","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_hashes","type":"bytes32[]"}],"name":"startAuctions","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"hash","type":"bytes32"},{"name":"deed","type":"address"},{"name":"registrationDate","type":"uint256"}],"name":"acceptRegistrarTransfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_hash","type":"bytes32"}],"name":"startAuction","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"rootNode","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"hashes","type":"bytes32[]"},{"name":"sealedBid","type":"bytes32"}],"name":"startAuctionsAndBid","outputs":[],"payable":true,"type":"function"},{"inputs":[{"name":"_ens","type":"address"},{"name":"_rootNode","type":"bytes32"},{"name":"_startDate","type":"uint256"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"hash","type":"bytes32"},{"indexed":false,"name":"registrationDate","type":"uint256"}],"name":"AuctionStarted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"hash","type":"bytes32"},{"indexed":true,"name":"bidder","type":"address"},{"indexed":false,"name":"deposit","type":"uint256"}],"name":"NewBid","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"hash","type":"bytes32"},{"indexed":true,"name":"owner","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"statu3s","type":"uint8"}],"name":"BidRevealed","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"hash","type":"bytes32"},{"indexed":true,"name":"owner","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"registrationDate","type":"uint256"}],"name":"HashRegistered","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"hash","type":"bytes32"},{"indexed":false,"name":"value","type":"uint256"}],"name":"HashReleased","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"hash","type":"bytes32"},{"indexed":true,"name":"name","type":"string"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"registrationDate","type":"uint256"}],"name":"HashInvalidated","type":"event"}]
 abiDecoder.addABI(registrarABI);
 
+// ----------------------------------------- MAIN SEQUENCE ----------------------------------------------//
+var ensData = {
+	"hash": '',
+	"owner": '',
+	"events": {
+		"revealedBids": [],
+		"finalized": false,
+		"released": false,
+		"started": false,
+		"transferred": false,
+	}
+}
+var ensDataArray = []
+var firstrun = false
+
+//read the previous file
+try{
+	data = fs.readFileSync('data.json')
+	ensData = JSON.parse(data)
+}
+catch(error){
+	firstrun = true
+}
+
+if (firstrun){
+	console.log("First Run Sequence")
+	web3.eth.getBlockNumber(function(err, blockNum){
+		console.log(blockNum)
+		processBlocks(registrar, 6079000, blockNum, function(err, ensData){
+			if(!err){
+				console.log("Block Processing Complete - " + ENS_hashes.length.toString() + " unique auctions found")
+				firstrun = false
+			}
+			else{
+				console.log("[ERROR] Could not process blocks")
+			}
+		})
+	})
+	//wait until first run sequence is completed
+	while (firstrun){sleep(100)}
+}
+
+console.log("hello")
+
+
+
+//----------------------------------------------------Helper Functions-----------------------------------------------//
 //check if unique
 Array.prototype.inArray = function(comparer) { 
     for(var i=0; i < this.length; i++) { 
@@ -49,21 +96,23 @@ function processBlocks(myaccount, startBlockNumber, endBlockNumber, callback) {
 		      		/*check if the to address matches with the account*/
 			        if (myaccount == e.to) {
 						var inputData = abiDecoder.decodeMethod(e.input)
-						if(inputData["name"] == 'unsealBid'){
-				          	params = inputData["params"]
+						var name = inputData["name"]
+						var params = inputData["params"]
+						// if(inputData["name"] == 'unsealBid'){
+				  //         	params = inputData["params"]
 
-				          	//create JSON data which stores the unsealBid attributes
-				          	unsealInfo["hash"] = params[0]["value"]
-				          	unsealInfo["from"] = e.from
-				          	unsealInfo["bidAmount"] = parseInt(params[1]["value"])
+				  //         	//create JSON data which stores the unsealBid attributes
+				  //         	unsealInfo["hash"] = params[0]["value"]
+				  //         	unsealInfo["from"] = e.from
+				  //         	unsealInfo["bidAmount"] = parseInt(params[1]["value"])
 
-				          	var unsealInfoJson = JSON.stringify(unsealInfo)
-				          	unsealedBids.push(unsealInfoJson)
-				          	//push only if it is not present in the array already
-				          	if(!ENS_hashes.inArray(params[0]["value"])){
-				          		ENS_hashes.push(params[0]["value"])
-				          	}
-						}
+				  //         	var unsealInfoJson = JSON.stringify(unsealInfo)
+				  //         	unsealedBids.push(unsealInfoJson)
+				  //         	//push only if it is not present in the array already
+				  //         	if(!ENS_hashes.inArray(params[0]["value"])){
+				  //         		ENS_hashes.push(params[0]["value"])
+				  //         	}
+						// }
 			        }
 			        if(numTxnProcessed == totalTxn){
 			        	numBlockProcessed++
@@ -91,36 +140,3 @@ function processBlocks(myaccount, startBlockNumber, endBlockNumber, callback) {
 	    })
   	}
 }
-
-// ----------------------------------------- MAIN SEQUENCE ----------------------------------------------//
-var ensData = {}
-var firstrun = false
-
-//read the previous file
-try{
-	data = fs.readFileSync('data.json')
-	ensData = JSON.parse(data)
-}
-catch(error){
-	firstrun = true
-}
-
-if (firstrun){
-	console.log("First Run Sequence")
-	web3.eth.getBlockNumber(function(err, blockNum){
-		console.log(blockNum)
-		processBlocks(registrar, 6079000, blockNum, function(err, ENS_hashes, unsealedBids){
-			if(!err){
-				console.log("Block Processing Complete - " + ENS_hashes.length.toString() + " unique auctions found")
-			}
-			else{
-				console.log("[ERROR] Could not process blocks")
-			}
-			firstrun = false
-		})
-	})
-	//wait until first run sequence is completed
-	while (firstrun){sleep(100)}
-}
-
-console.log("hello")
